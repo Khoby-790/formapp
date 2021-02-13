62139cc92ad90236d30b5f072f5e23c9743979df
Khoby-790 <ebaidoo79@gmail.com>
2021-02-13 22:57:30 +0000

    Changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import 'antd/dist/antd.css'; 
 import './index.css';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,4 +10,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-// reportWebVitals();
