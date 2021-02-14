2a56bd2c82bc29f4f06773691ced049cf5660359
Khoby-790 <ebaidoo79@gmail.com>
2021-02-14 00:49:00 +0000

    --- Auto-Git Commit ---

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,11 +1,11 @@
 import React, { forwardRef } from 'react'
 
-const FormInput = forwardRef(({ name, type = "text", label, showError, errors = {}, ...rest }, ref) => {
+const FormInput = forwardRef(({ name, type = "text", label, showError, errors = null, ...rest }, ref) => {
     return (
         <div className="flex flex-1 flex-col">
             <label htmlFor={name}>{label}</label>
             <input className="h-11 border px-2 outline-none" name={name} type={type} {...rest} ref={ref} />
-            {showError && errors[name] && <p className="text-red-600">{errors[name]?.message}</p>}
+            {errors && errors[name] && <p className="text-red-600">{errors[name]?.message}</p>}
         </div>
     )
 })
