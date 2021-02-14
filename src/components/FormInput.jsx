33ef9c8c6a294034efb58ada00d1d45e6b2efdd5
Khoby-790 <ebaidoo79@gmail.com>
2021-02-14 00:29:38 +0000

    --- Auto-Git Commit ---

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -4,7 +4,7 @@ const FormInput = forwardRef(({ name, type = "text", label, ...rest }, ref) => {
     return (
         <div className="flex flex-1 flex-col">
             <label htmlFor={name}>{label}</label>
-            <input name={name} type={type} {...rest} ref={ref} />
+            <input className="h-11 border px-2 outline-none" name={name} type={type} {...rest} ref={ref} />
         </div>
     )
 })
