56ff5ce4cb8d947a5c871340f048a8ef3c2e70c5
Khoby-790 <ebaidoo79@gmail.com>
2021-02-14 00:50:51 +0000

    --- Auto-Git Commit ---

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -4,8 +4,8 @@ const FormInput = forwardRef(({ name, type = "text", label, showError, errors = 
     const hasError = errors && errors[name];
     return (
         <div className="flex flex-1 flex-col">
-            <label htmlFor={name}>{label}</label>
-            <input className="h-11 border px-2 outline-none" name={name} type={type} {...rest} ref={ref} />
+            <label className={`${hasError ? "text-red-600" : ""}`} htmlFor={name}>{label}</label>
+            <input className={`h-11 border px-2 outline-none ${hasError ? "border-red-500" : ""}`} name={name} type={type} {...rest} ref={ref} />
             { hasError && <p className="text-red-600">{errors[name]?.message}</p>}
         </div>
     )
