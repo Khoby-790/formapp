299b6c05ff5b9d4ab484fa30bc71b34c69474f26
Khoby-790 <ebaidoo79@gmail.com>
2021-02-15 11:53:36 +0000

    --- Auto-Git Commit ---

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
         <div className="px-9 bg-gray-300  flex-1 overflow-scroll grid  sm:grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-5">
           <AddNewForm />
           {/* Single Component */}
-          {todos.map((quiz, key) => <FormItem key={key} />)}
+          {todos.map((quiz: any, key: number) => <FormItem key={key} />)}
         </div>
       </div>
 
