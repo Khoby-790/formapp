c6aacb62f13729a6594e262394e41b419884a028
Khoby-790 <ebaidoo79@gmail.com>
2021-02-15 16:37:41 +0000

    --- Auto-Git Commit ---

--- src/components/FormItem.tsx
+++ src/components/FormItem.tsx
@@ -14,7 +14,15 @@ const FormItem = ({ form }: FormItemType) => {
                 <div className="absolute rounded-bl-lg  bg-gray-600 cursor-pointer w-11 flex justify-center items-center h-8 right-0">
                     <BsThreeDots onClick={() => setShowmenu(true)} color="#fff" size={30} />
                     <div className="relative">
-                        <Transition show={showmenu}>
+                        <Transition
+                            show={showmenu}
+                            enter="transition ease-out duration-100"
+                            enterFrom="transform opacity-0 scale-95"
+                            enterTo="transform opacity-100 scale-100"
+                            leave="transition ease-in duration-75"
+                            leaveFrom="transform opacity-100 scale-100"
+                            leaveTo="transform opacity-0 scale-95"
+                        >
                             <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
                                 <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
                                     <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Account settings</a>
