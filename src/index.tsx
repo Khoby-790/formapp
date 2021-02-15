7a2e2b1b5a0813faad91ef4a37b92178e54ccd4e
Khoby-790 <ebaidoo79@gmail.com>
2021-02-15 12:53:33 +0000

    --- Auto-Git Commit ---

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import 'antd/dist/antd.css';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { store } from './redux';
 
 ReactDOM.render(
   <Provider store={store}>
--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,3 +1,8 @@
 import { createStore, applyMiddleware, Store } from "redux";
 import thunk from "redux-thunk";
 
+import reducer from "./reducers/quizReducers";
+
+export const store: Store<AppState, AppAction> & {
+  dispatch: DispatchType;
+} = createStore(reducer, applyMiddleware(thunk));
