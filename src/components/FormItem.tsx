29c3357f3c13acbef9111d1289d383b0be9cc712
Khoby-790 <ebaidoo79@gmail.com>
2021-02-15 16:34:00 +0000

    --- Auto-Git Commit ---

--- src/components/FormItem.tsx
+++ src/components/FormItem.tsx
@@ -11,6 +11,18 @@ const FormItem = ({ form }: FormItemType) => {
             <div className="h-64 relative mx-3 mb-2 bg-white">
                 <div className="absolute rounded-bl-lg  bg-gray-600 cursor-pointer w-11 flex justify-center items-center h-8 right-0">
                     <BsThreeDots color="#fff" size={30} />
+                    <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                        <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Account settings</a>
+                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Support</a>
+                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">License</a>
+                            <form method="POST" action="#">
+                                <button type="submit" className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">
+                                    Sign out
+                                </button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
                 <div className="bg-green-600 w-full max-h-24 h-24"></div>
                 <div className="h-40  w-full px-2 flex justify-between flex-col">
