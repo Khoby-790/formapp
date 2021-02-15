83c56c58c2046c3891a12726b7f7c5d7aa1233e2
Khoby-790 <ebaidoo79@gmail.com>
2021-02-15 12:30:29 +0000

    --- Auto-Git Commit ---

--- src/redux/reducers/quizReducers.ts
+++ src/redux/reducers/quizReducers.ts
@@ -1,5 +1,18 @@
+import { ADD_QUIZ } from "../types/quizTypes";
+
 const initialState: QuizesState = {
   quizes: [],
 };
 
+const reducer = (state: QuizesState = initialState, action: QuizAction) => {
+  switch (action.type) {
+    case ADD_QUIZ:
+      return {
+        ...state,
+        quizes: action.quiz,
+      };
 
+    default:
+      return state;
+  }
+};
