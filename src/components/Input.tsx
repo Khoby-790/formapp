032ef56ecb378c2dee992a86d6e5183e9b2408ee
Khoby-790 <ebaidoo79@gmail.com>
2021-02-15 16:17:23 +0000

    --- Auto-Git Commit ---

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -11,13 +11,14 @@ interface Props {
     errors?: FieldErrors
 }
 
-const Input = forwardRef<InputType, Props & InputType>(({ errors, name, type, ...rest }, ref) => {
+const Input = forwardRef<InputType, Props & InputType>(({ errors, name, type, label, ...rest }, ref) => {
     const hasError = name && errors && errors[name];
+    const errorMessage = hasError && errors && name && errors[name]?.message
     return (
         <div className="flex flex-1 flex-col">
-            <label className={`${hasError ? "text-red-600" : ""}`} htmlFor={name}>{label}</label>
+            {label && <label className={`${hasError ? "text-red-600" : ""}`} htmlFor={name}>{label}</label>}
             <input className={`h-11 border px-2 outline-none ${hasError ? "border-red-500" : ""}`} name={name} type={type} {...rest} ref={ref} />
-            { hasError && <p className="text-red-600">{errors[name]?.message}</p>}
+            { hasError && <p className="text-red-600">{errorMessage}</p>}
         </div>
     )
 })
