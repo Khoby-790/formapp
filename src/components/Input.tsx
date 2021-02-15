019a2fffe0945efbf7706ade4dd1a08a742ee406
Khoby-790 <ebaidoo79@gmail.com>
2021-02-15 16:22:00 +0000

    --- Auto-Git Commit ---

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,16 +2,16 @@ import React, { forwardRef } from 'react';
 import { FieldErrors } from 'react-hook-form';
 
 
-type InputType = React.HTMLProps<HTMLInputElement | HTMLLabelElement | HTMLTextAreaElement | HTMLSelectElement>
-
+type InputType = React.ClassAttributes<HTMLInputElement | HTMLLabelElement | HTMLTextAreaElement | HTMLSelectElement>
+type InputProps = React.HTMLProps<HTMLInputElement | HTMLLabelElement | HTMLTextAreaElement | HTMLSelectElement>
 interface Props {
-    // type?: String;
-    name?: String;
+    type?: string;
+    name?: string;
     label?: String;
     errors?: FieldErrors
 }
 
-const Input = forwardRef<InputType, Props & InputType>(({ errors, name, type, label, ...rest }, ref) => {
+const Input = forwardRef<HTMLInputElement, Props & InputType>(({ errors, name, type, label, ...rest }, ref) => {
     const hasError = name && errors && errors[name];
     const errorMessage = hasError && errors && name && errors[name]?.message
     return (
