1f37d3cc65bb0e8cc5be805a3f094890177b7fe8
Khoby-790 <ebaidoo79@gmail.com>
2021-02-15 11:27:12 +0000

    --- Auto-Git Commit ---

--- src/redux/reducers/quizApp.ts
+++ src/redux/reducers/quizApp.ts
@@ -1,17 +1,17 @@
 import { ADD_QUIZ } from "../types";
 
 const initialState = {
-    todos: [1]
-}
+  todos: [1],
+};
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (state = initialState, action) => {
-    switch (action.type) {
-        case ADD_QUIZ:
-            return {
-                todos: [...state.todos, action.payload]
-            }
-        default:
-            return state
-    }
-}
+export default (state = initialState, action: any) => {
+  switch (action.type) {
+    case ADD_QUIZ:
+      return {
+        todos: [...state.todos, action.payload],
+      };
+    default:
+      return state;
+  }
+};
