bf5a6927b446a7cfd2b11c9dfc7bbfffe40eb38f
Khoby-790 <ebaidoo79@gmail.com>
2021-02-15 13:44:47 +0000

    --- Auto-Git Commit ---

--- src/redux/actions/FormActions.ts
+++ src/redux/actions/FormActions.ts
@@ -1,1 +1,12 @@
-import {} from '../types/formTypes';
+import { ADD_FORM } from "../types/formTypes";
+
+export function addForm(form: IForm) {
+  const action: FormAction = {
+    type: ADD_FORM,
+    form,
+  };
+
+  return (dispatch: DispatchType) => {
+    dispatch(action);
+  };
+}
